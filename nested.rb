071bfa422765431def6c03cc18f8c4eaaa105560
Erin Walker <erinb0317@hotmail.com>
2018-01-31 00:48:07 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -126,5 +126,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] = "Assembly"
-end
+		 dennis_new_info = "Assembly"
+		 programmer_hash[:dennis_ritchie][:languages] = dennis_new_info
+		 programmer_hash
+	 end
